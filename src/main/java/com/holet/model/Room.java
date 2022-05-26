c7405996b23f58128f5d65baf3fee7ec37d8265c
Brijesh Kumar Dubey <brijeshkumar.dubey@transcendstreet.com>
2022-05-26 12:34:17 +0530

    hotel management

--- src/main/java/com/holet/model/Room.java
+++ src/main/java/com/holet/model/Room.java
@@ -1,5 +1,9 @@
 package com.holet.model;
 
 public class Room {
-
+	int roomNumber;
+	RoomStyle roomStyle;
+	Double bookingPrice;
+	List<RoomKey> roomKeys;
+	List<HouseKeepingLog> houseKeepingLogs;
 }
